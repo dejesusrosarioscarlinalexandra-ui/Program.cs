95593486593acb9a7659dcfb69781803219283ef
dejesusrosarioscarlinalexandra-ui <dejesusrosarioscarlinalexandra@gmail.com>
2026-02-11 00:31:16 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,71 +1,25 @@
-﻿using System;
-using Spectre.Console;
+﻿
+// Variables y principales tipos de datos
+string nombre;
+string apellido;
+string color;
+int edad;
+float altura;
 
+// Entrada de datos
+Console.Write("Nombre: ");
+nombre = Console.ReadLine()!;
 
-class Program
-{
-static void Main()
-    {
-        // Solicitar datos
-        Console.Write("Ingrese el monto del préstamo: ");
-        decimal monto = decimal.Parse(Console.ReadLine()!);
+Console.Write("Apellido: ");
+apellido = Console.ReadLine()!;
 
-        Console.Write("Ingrese el interés anual: ");
-        decimal interesAnual = decimal.Parse(Console.ReadLine()!);
+Console.Write("Edad: ");
+edad = int.Parse(Console.ReadLine()!);
 
-        Console.Write("Ingrese el plazo mensual: ");
-        int plazo = int.Parse(Console.ReadLine()!);
+Console.Write("Altura: ");
+altura = float.Parse(Console.ReadLine()!);
 
-        // Convertir a interés mensual
-        decimal interesMensual = (interesAnual / 12) / 100;
-
-        // Calcular cuota fija
-        decimal potencia = (decimal)Math.Pow(1 + (double)interesMensual, plazo);
-
-        decimal cuota = monto *
-                        (interesMensual * potencia) /
-                        (potencia - 1);
-
-        cuota = Math.Round(cuota, 2);
-
-        // Crear tabla
-        Table tabla = new Table();
-
-        tabla.AddColumn("Cuota");
-        tabla.AddColumn("Pago");
-        tabla.AddColumn("Interés");
-        tabla.AddColumn("Abono");
-        tabla.AddColumn("Saldo");
-
-        decimal saldo = monto;
-
-        // Generar tabla
-        for (int i = 1; i <= plazo; i++)
-        {
-            decimal interes = saldo * interesMensual;
-            interes = Math.Round(interes, 2);
-
-            decimal abono = cuota - interes;
-            abono = Math.Round(abono, 2);
-
-            saldo -= abono;
-            saldo = Math.Round(saldo, 2);
-
-            if (saldo < 0)
-                saldo = 0;
-
-            tabla.AddRow(
-                i.ToString(),
-                cuota.ToString("N2"),
-                interes.ToString("N2"),
-                abono.ToString("N2"),
-                saldo.ToString("N2")
-            );
-        }
-
-        // Mostrar tabla
-        AnsiConsole.Write(tabla);
-
-        Console.ReadKey();
-    }
-}
+// Salida final
+Console.WriteLine($"\nHola, mi nombre es {nombre} {apellido}");
+Console.WriteLine($"Tengo {edad} años de edad");
+Console.WriteLine($"Mi altura es {altura}");
