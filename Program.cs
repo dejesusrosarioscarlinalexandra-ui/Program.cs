e1920a87aedcbb733b7a55b25c9c9b3a6498bb0e
dejesusrosarioscarlinalexandra-ui <dejesusrosarioscarlinalexandra@gmail.com>
2026-02-12 03:49:04 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Variables y principales tipos de datos
+string nombre;
+string apellido;
+int edad;
+float altura;
+
+// Entrada de datos
+Console.Write("Nombre: ");
+nombre = Console.ReadLine()!;
+
+Console.Write("Apellido: ");
+apellido = Console.ReadLine()!;
+
+Console.Write("Edad: ");
+edad = int.Parse(Console.ReadLine()!);
+
+Console.Write("Altura: ");
+altura = float.Parse(Console.ReadLine()!);
+
+// Salida final
+Console.WriteLine($"\nHola, mi nombre es {nombre} {apellido}");
+Console.WriteLine($"Tengo {edad} años de edad");
+Console.WriteLine($"Mi altura es {altura}");
