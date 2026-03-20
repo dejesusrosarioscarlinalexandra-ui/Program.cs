bce375f65cb4481ffa20da9ff00408e4dbe66317
dejesusrosarioscarlinalexandra-ui <dejesusrosarioscarlinalexandra@gmail.com>
2026-03-20 00:44:06 -0400

    Delete MiembroModel.cs

--- MiembroModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Bookcase.Models;
-
-public class MiembroModel
-{
-    public int Id { get; set; }
-    public string NombreCompleto { get; set; } = "";
-    public string Cedula { get; set; } = "";
-    public string Telefono { get; set; } = "";
-}
